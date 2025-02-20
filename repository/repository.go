e20b1fd6b4ecdee8de224da1366d760d648bf657
sandit-coder <egor2009vit@gmail.com>
2025-02-20 17:13:34 +0300

    Update repository.go

--- repository/repository.go
+++ repository/repository.go
@@ -4,10 +4,10 @@ import (
 	"container/list"
 	"database/sql"
 
-	"github.com/lakshanwd/go-crud/db"
+	
 )
 
-//Repo - abstract repository interface
+
 type Repo interface {
 	Select(doc interface{}) (*list.List, error)
 	Insert(doc interface{}) (int64, error)
@@ -15,16 +15,16 @@ type Repo interface {
 	Remove(doc interface{}) (int64, error)
 }
 
-//DbConnection - Database Connectin Pool
+
 var DbConnection *sql.DB
 
-//SetupRepo - setup database connections
+
 func SetupRepo() (err error) {
 	DbConnection, err = db.GetDatabase()
 	return
 }
 
-//CloseRepo - close database connections
+
 func CloseRepo() {
 	if DbConnection != nil {
 		defer DbConnection.Close()
